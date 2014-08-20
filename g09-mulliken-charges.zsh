abfd0073218fd98a6b99e7160948307188c5d1d5
Chauncey Garrett <vrtcl1dvoshun+github@gmail.com>
2014-08-20 11:45:55 -0500

    Testing support for Gaussian 09 rev D
    [ticket: X]

--- g09-mulliken-charges.zsh
+++ g09-mulliken-charges.zsh
@@ -6,32 +6,68 @@ set  -f
 
 function g09-mulliken-charges()
 {
-	if [[ -z "${2}" ]]
-	then # all Mulliken charges are requested
+	if [[ $(${GAUSSIAN_TOOLS_DIRECTORY:-$HOME/bin}/g09-revision.zsh "${1}") == "Gaussian 09, Revision D.01" ]]
+	then # using Gaussian 09, Revision D.01
 
-		data=$(
-			awk '/Mulliken charges and spin densities:/,/Sum of Mulliken charges/ { print }' "${1}" |\
-				head -n -1 |\
-				tac |\
-				sed -n '1,/               1          2/p' |\
-				head -n -1 |\
-				tac |\
-				awk '{print $2, $3}' |\
-				column -t
-		)
+		if [[ -z "${2}" ]]
+		then # all Mulliken charges are requested
 
-	else # Mulliken charges of a specific atom are requested
+			data=$(
+				awk '/Mulliken charges and spin densities:/,/Sum of Mulliken charges/ { print }' "${1}" |\
+					head -n -1 |\
+					tac |\
+					sed -n '1,/               1          2/p' |\
+					head -n -1 |\
+					tac |\
+					awk '{print $2, $3}' |\
+					column -t
+			)
 
-		data=$(
-			awk '/Mulliken charges and spin densities:/,/Sum of Mulliken charges/ { print }' "${1}" |\
-				head -n -1 |\
-				tac |\
-				sed -n '1,/               1          2/p' |\
-				head -n -1 |\
-				tac |\
-				awk -v atom="${2}" '$0 ~ atom {print $3}'
-		)
+		else # Mulliken charges of a specific atom are requested
 
+			data=$(
+				awk '/Mulliken charges and spin densities:/,/Sum of Mulliken charges/ { print }' "${1}" |\
+					head -n -1 |\
+					tac |\
+					sed -n '1,/               1          2/p' |\
+					head -n -1 |\
+					tac |\
+					awk -v atom="${2}" '$0 ~ atom {print $3}'
+			)
+
+		fi
+
+	elif [[ $(${GAUSSIAN_TOOLS_DIRECTORY:-$HOME/bin}/g09-revision.zsh "${1}") == "Gaussian 09, Revision C.01" ]]
+	then # using Gaussian 09, Revision C.01
+
+		if [[ -z "${2}" ]]
+		then # all Mulliken charges are requested
+
+			data=$(
+				awk '/Mulliken atomic charges:/,/Sum of Mulliken atomic charges/ { print }' "${1}" |\
+					head -n -1 |\
+					tac |\
+					sed -n '1,/             1/p' |\
+					head -n -1 |\
+					tac |\
+					awk '{print $2, $3}' |\
+					column -t
+			)
+
+		else # Mulliken charges of a specific atom are requested
+
+			data=$(
+				awk '/Mulliken atomic charges:/,/Sum of Mulliken atomic charges/ { print }' "${1}" |\
+					head -n -1 |\
+					tac |\
+					sed -n '1,/             1/p' |\
+					head -n -1 |\
+					tac |\
+					awk -v atom="${2}" '$0 ~ atom {print $3}'
+			)
+		fi
+	else # perhaps unsupported?
+		data="Gaussian revision unsupported"
 	fi
 
 	"${GAUSSIAN_TOOLS_DIRECTORY:-$HOME/bin}/g09-data-test.zsh" "$data"
